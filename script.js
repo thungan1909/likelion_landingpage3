464b16f8bab26cdf299e870086055cbfa404a48a
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2022-12-26 14:41:43 +0700

    apply responsive and fix some bug

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 const _likeBtn = document.querySelectorAll(".likeBtn");
 
 
-
-
-// _likeBtn.addEventListener("click", function () {
-//     _likeIcon.classList.replace("fa-thumbs-up", "fa-check");
-// })
-
-
 _likeBtn.forEach( (btn) => {
     btn.addEventListener("click", function () {
         btn.children[0].classList.replace("fa-thumbs-up", "fa-check");
         btn.children[1].textContent = "Liked"; 
     });
 })
+
+const comments = document.querySelectorAll(".comments");
+const replies = document.querySelectorAll(".reply");
+
+replies.forEach((btn, index) => {
+    btn.addEventListener("click", function() {
+        comments[index].classList.toggle("d-none");
+    });
+});
